0656385f26fe8e67327701c3cdec279fdb3af603
Katherine de Vay <katdevay2@gmail.com>
2023-03-15 17:08:30 -0500

    Build card component & main div

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import './App.css';
 
-function App() {
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Card from './components/Card';
+
+export default function App() {
+  const {cards, setCards} = useState([{id: 'card1', balance: 0, limit: 0 }]);
+
+  const addCard = () => {
+    
+  }
+
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <h1 className='mainHeader'>Credit Utilization Calculator</h1>
+      <div className='calculator'>
+        <form>
+          {/* {
+            cards.forEach(( card, i ) => {
+              return <Card index={i}></Card>
+            })
+          } */}
+          
+        </form>
+      </div>
     </div>
   );
 }
 
-export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Card(props) {
+    const { index } = props;
+    const doSomething = () => {
+        console.log('clicked help button');
+    };
+
+    return (
+        <div className='card'>
+            <label for={'balance' + index}>{'Card' + index + 'balance'}</label>
+            <input id={'balance' + index}></input>
+            <div className='inputlabelDiv'>
+                <label for={'limit' + index}>{'Card' + index + 'limit'}</label>
+                <button className='helpButton' onClick={doSomething}>?</button>
+            </div>
+            <input id={'limit' + index}></input>
+        </div>
+    );
+}
